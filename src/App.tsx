77137a12f65abb7cc2852efaa0a8aad265431fe6
Gabriel von Platen <gabrielvonplaten@gmail.com>
2020-06-26 00:47:43 +0200

    Create name container and animation on load

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './Components/Home/Home';
-import About from './Components/About/About';
-import Navbar from './Components/Navbar/Navbar';
+import { Home } from './Components/Home/Home';
+import { About } from './Components/About/About';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <div>
-        <Navbar />
         <Switch>
           <Route exact={true} path='/' component={Home} />
-          <Route exact={true} path='/about' component={About} />
+          <Route exact={true} path='/work' component={About} />
         </Switch>
       </div>
     </BrowserRouter>
--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,20 +1,56 @@
-import React, { useState } from 'react';
-import Logo from '../../Styles/images/logo.svg';
+import React, { useEffect } from 'react';
 import './Home.sass';
 
-const Home: React.FC = () => {
-  const [count, setData] = useState<number>(0);
+export const Home: React.FC = () => {
+  useEffect(() => {
+    const elements: any = document.querySelectorAll('.anim');
+
+    const observer = new IntersectionObserver(
+      (entries: any) => {
+        entries.forEach((entry: any) => {
+          if (entry.intersectionRatio > 0) {
+            entry.target.style.animation = `anim_one 2.6s ${entry.target.dataset.delay} forwards ease-out`;
+          }
+        });
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0,
+      },
+    );
+
+    elements.forEach((el: any) => {
+      observer.observe(el);
+    });
+  });
 
   return (
-    <div className='app'>
-      <img className='app__logo' src={Logo} />
-      <h1 className='title'>Hello React + Typescript!</h1>
-      <button onClick={() => setData(count + 1)}>+</button>
-      <button onClick={() => setData(count - 1)}>-</button>
-      <button onClick={() => setData(0)}>Reset</button>
-      <p>Count: {count}</p>
+    <div className='name anim' data-delay='1'>
+      <h3 className='name__profession'>
+        Frontend Developer / Stockholm, Sweden
+      </h3>
+      <h1 className='name__gabriel anim' data-delay='1'>
+        Gabriel von Platen
+      </h1>
+      <div className='name__line anim' data-delay='1' />
+      <div className='name__about'>
+        <p>
+          Veniam anim nostrud et commodo irure duis culpa eu mollit dolore
+          magna. Nostrud magna ipsum ea pariatur id nostrud est consequat
+          incididunt do ea. Aliqua nostrud amet irure eiusmod est reprehenderit
+          occaecat ea cillum velit reprehenderit non dolore sit. Pariatur sunt
+          nulla aliquip minim ut proident eiusmod.
+        </p>
+      </div>
+      <a
+        href='https://www.docdroid.net/J5U2Pxs/cv-pdf'
+        target='_blank'
+        className='btn btn--orange name__resume-btn anim'
+        data-delay='1'
+      >
+        My Resume
+      </a>
     </div>
   );
 };
-
-export default Home;

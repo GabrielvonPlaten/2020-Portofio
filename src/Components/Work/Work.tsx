3a4cc087c8eafc079c80907a23d200f2597037f6
Gabriel von Platen <gabrielvonplaten@gmail.com>
2020-06-26 20:34:05 +0200

    Display works (incomplete)

--- src/Components/Work/Work.tsx
+++ src/Components/Work/Work.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import './Work.sass';
 
+import workData from '../../data/works.json';
+import { url } from 'inspector';
+
 export const Work: React.FC = () => {
   return (
-    <div>
-      <h1 className='work__title'>work</h1>
+    <div className='work-container'>
+      {workData.works.map((work: any) => (
+        <div
+          className='work-container__work'
+          style={{ backgroundImage: `url(${work.thumbnail})` }}
+        >
+          <div className='work-darkBackground' />
+          <div className='work-about'>
+            <h2>{work.name}</h2>
+            <p>{work.description}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };

fca61b2a85eae2ddef2b22412a525a253170d80e
Gabriel von Platen <gabrielvonplaten@gmail.com>
2020-06-26 21:17:40 +0200

    Finish work section

--- src/Components/Work/Work.tsx
+++ src/Components/Work/Work.tsx
@@ -15,7 +15,26 @@ export const Work: React.FC = () => {
           <div className='work-darkBackground' />
           <div className='work-about'>
             <h2>{work.name}</h2>
+            <ul className='tech-list'>
+              {work.tech.map((tech: any) => (
+                <li className='tech-list__item'>{tech.name}</li>
+              ))}
+            </ul>
             <p>{work.description}</p>
+            <ul className='links-list'>
+              {work?.github && (
+                <li>
+                  <a className='btn btn--small' href={work.github}>
+                    Github
+                  </a>
+                </li>
+              )}
+              <li>
+                <a className='btn btn--small' href={work.website}>
+                  Website
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
       ))}
